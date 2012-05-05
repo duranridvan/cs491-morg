1b512c3064f0593bc646217c4e000ed06280731e
ozankar@gmail.com <ozankar@gmail.com@853600c4-34a3-4784-74ae-96983725d84f>
2012-05-05 22:43:23 +0000

    Path

---
+++ com.aselsan.targettracking/src/com/aselsan/targettracking/Path.java
@@ -0,0 +1,43 @@
+package com.aselsan.targettracking;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import org.eclipse.swt.graphics.Point;
+
+public class Path {
+	
+	ArrayList<Point> points;
+	String startTime, finishTime;
+	
+	public Path(){
+		points = new ArrayList<Point>();
+		startTime = DateUtils.now();
+	}
+	
+	public void addPoint(Point p){
+		points.add(p);
+	}
+	public void finishPath(){
+		finishTime = DateUtils.now();
+	}
+
+	public static class DateUtils {
+		  public static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
+
+		  public static String now() {
+		    Calendar cal = Calendar.getInstance();
+		    SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
+		    return sdf.format(cal.getTime());
+
+		  }
+
+		  public static void  main(String arg[]) {
+		    System.out.println("Now : " + DateUtils.now());
+		  }
+		}
+}
+
+
+
